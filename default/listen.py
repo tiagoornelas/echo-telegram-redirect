e9fa5a126ad539a029db3e7d794cc2c82fcbbc8d
tiagoornelas <advtiagoornelas@gmail.com>
2022-11-08 20:34:14 -0300

    feat(bot): adds tip manager bot for new ornelas bot

--- default/listen.py
+++ default/listen.py
@@ -1,6 +1,8 @@
 from echo import echo_listen
 
 listen_to = [
+    # Ornelas Bot
+    2022773493,
     # Mestre Manager
     -1001729121754,
     # Alertas FIFA
@@ -9,7 +11,7 @@ listen_to = [
     -1001660066336,
 ]
 
-channel_name_by_id = {1729121754: "Mestre Manager", 1793865102: "GD", 1660066336: "Debugger"}
+channel_name_by_id = {2022773493: "Ornelas Bot", 1729121754: "Mestre Manager", 1793865102: "GD", 1660066336: "Debugger"}
 
 # Entradas - Echo Telegram
 target_channel = -1001790572391
