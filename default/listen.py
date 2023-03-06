bb6c4c179b0b76a58ce5b759a20f0336f5e83305
Tiago Ornelas <advtiagoornelas@gmail.com>
2023-03-06 11:25:14 -0300

    fix(listen): corrects the right listen file

--- default/listen.py
+++ default/listen.py
@@ -1,18 +1,15 @@
 from echo import echo_listen
 
 listen_to = [
-    # Mestre Manager
-    -1001729121754,
+    # Ornelas Bot
+    2022773493,
     # Debugger Chat
     -1001660066336,
 ]
 
-channel_name_by_id = {2022773493: "Ornelas Bot", 1729121754: "Mestre Manager", 1793865102: "GD", 1660066336: "Debugger"}
+channel_name_by_id = {2022773493: "Ornelas Bot", 1660066336: "Debugger"}
 
 # Entradas - Echo Telegram
-ornelas_source_channel_id = 2022773493
+target_channel = -1001790572391
 
-default_target_channel = -1001790572391
-ornelas_target_channel = -1001684445367
-
-echo_listen(listen_to, channel_name_by_id, default_target_channel, ornelas_source_channel_id, ornelas_target_channel)
+echo_listen(listen_to, channel_name_by_id, target_channel)
--- split_ornelas/listen.py
+++ split_ornelas/listen.py
@@ -1,15 +1,18 @@
 from echo import echo_listen
 
 listen_to = [
-    # Ornelas Bot
-    2022773493,
+    # Mestre Manager
+    -1001729121754,
     # Debugger Chat
     -1001660066336,
 ]
 
-channel_name_by_id = {2022773493: "Ornelas Bot", 1660066336: "Debugger"}
+channel_name_by_id = {2022773493: "Ornelas Bot", 1729121754: "Mestre Manager", 1793865102: "GD", 1660066336: "Debugger"}
 
 # Entradas - Echo Telegram
-target_channel = -1001790572391
+ornelas_source_channel_id = 2022773493
 
-echo_listen(listen_to, channel_name_by_id, target_channel)
+default_target_channel = -1001790572391
+ornelas_target_channel = -1001684445367
+
+echo_listen(listen_to, channel_name_by_id, default_target_channel, ornelas_source_channel_id, ornelas_target_channel)
