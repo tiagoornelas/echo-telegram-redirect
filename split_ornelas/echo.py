5f6b36961ad8177bc35501e9b4f462f1addf3694
Tiago Ornelas <advtiagoornelas@gmail.com>
2023-03-06 11:28:34 -0300

    fix(echo): fixes wrong parameter variable

--- split_ornelas/echo.py
+++ split_ornelas/echo.py
@@ -53,7 +53,7 @@ def echo_listen(from_chats, channel_names_by_id, default_chat, ornelas_source_ch
                 message = message.replace("Tip Manager", "DataAPI")
             # Sends text only when there is no image
             if event.message.media is None or isinstance(event.message.media, types.MessageMediaWebPage):
-                await send_message_to_dynamic_channels(default_chat, ornelas_source_id, ornelas_chat, message, event)
+                await send_message_to_dynamic_channels(default_chat, ornelas_source_channel_id, ornelas_chat, message, event)
             # Sends image with caption
             # else:
             #     return await client.send_file(entity=default_chat, file=event.message.media, caption=message)
