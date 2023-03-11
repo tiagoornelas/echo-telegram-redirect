4e3845c61eaf75f80edf07afbef62188fb5eeee7
Tiago Ornelas <advtiagoornelas@gmail.com>
2023-03-11 14:16:58 -0300

    delete(split_ornelas): keeps specific settings to another branches, in order to keep main branch simple

--- split_ornelas/client.py
+++
@@ -1,7 +0,0 @@
-from telethon.sync import TelegramClient
-
-API_ID = 11670226
-API_NAME = "EchoTel"
-API_HASH = "58af5da7ff696e74a7dce4c1accf2a18"
-
-client = TelegramClient(API_NAME, API_ID, API_HASH)
