8de1c0cbd2d218eb9b97b2e9e0a1f77294296558
Tiago Ornelas <advtiagoornelas@gmail.com>
2023-05-03 06:53:41 -0300

    feat(sanitize-tipmanager): changes sanitize function to avoid expired messages and one access only links

--- src/telegram.py
+++ src/telegram.py
@@ -43,8 +43,13 @@ def log_error(message, chat_settings, error):
     })
 
 def sanitize_tipmanager_messages(message):
+    if "Poxa, que pena" in message:
+        return ""
     message = f"{message.replace('tipmanager', 'dataapi')}"
     message = f"{message.split('———')[0]}"
+    if 'https://bot.' in message:
+        links = f"[Battle](https://www.bet365.com/#/AC/B1/C1/D1002/E47578773/G938/)|[Arena](https://www.bet365.com/#/AC/B1/C1/D1002/E72260052/G938/)|[GT](https://www.bet365.com/#/AC/B1/C1/D1002/E71755872/G938/)"
+        message = f"{message.split('https://bot.')[0]}\n\n{links}"
     return message
 
 def prompt_for_chat_settings(client):
