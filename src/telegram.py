6066484d0daadfcceb88deb6bfc040da264ec4e4
Tiago Ornelas <advtiagoornelas@gmail.com>
2023-09-04 20:53:35 -0300

    feat (delayed messages)
    Creates possibility to send alternative message to delayed chat with percentage chance

--- src/telegram.py
+++ src/telegram.py
@@ -1,11 +1,12 @@
 import os
 import datetime
+import asyncio
+import random
 import inquirer
 from telethon.tl import types
 from dotenv import load_dotenv
 from telethon.sync import events
 from telethon.sync import TelegramClient
-from pymongo import MongoClient
 
 
 def log_app_initialization(chat_settings):
@@ -19,28 +20,11 @@ def log_app_initialization(chat_settings):
         print("Tip Manager's messages sanitizer is enabled")
 
 
-def log_sent_message(message, chat_settings):
+def log_sent_message(message):
     print(f"{datetime.datetime.utcnow()} - {message}")
-    db.messages.insert_one({
-        'from_app': 'Telegram',
-        'to_app': 'Telegram',
-        'from': chat_settings['source_chats_titles'],
-        'to': chat_settings['recipient_chat_title'],
-        'message': message,
-        'datetime': datetime.datetime.utcnow()
-    })
 
-def log_error(message, chat_settings, error):
+def log_error(error):
     print(f"{datetime.datetime.utcnow()} - ERROR: {error}")
-    db.errors.insert_one({
-        'from_app': 'Telegram',
-        'to_app': 'Telegram',
-        'from': chat_settings['source_chats_titles'],
-        'to': chat_settings['recipient_chat_title'],
-        'message': message,
-        'error': error,
-        'datetime': datetime.datetime.utcnow()
-    })
 
 def should_block_strategy_by_black_list(message):
     black_list = ["Next Goal"]
@@ -125,11 +109,13 @@ def prompt_for_chat_settings(client):
     questions = [
         inquirer.Checkbox('source', message='Select source chats', choices=chat_choices),
         inquirer.List('recipient', message='Select recipient chat', choices=chat_choices),
-        inquirer.Confirm('should_sanitize', message="Do you want to sanitize Tip Manager's messages?", default=False)
+        inquirer.Confirm('should_sanitize', message="Do you want to sanitize Tip Manager's messages?", default=False),
+        inquirer.Confirm('should_send_delayed', message="Do you want to send the same message for another chat with delay?", default=False)
     ]
 
     answers = inquirer.prompt(questions)
     should_sanitize = answers['should_sanitize']
+    should_send_delayed = answers['should_send_delayed']
 
     source_chats_titles = answers['source']
     recipient_chat_title = answers['recipient']
@@ -140,20 +126,47 @@ def prompt_for_chat_settings(client):
 
     recipient_chat = chat_dict[recipient_chat_title]
 
+    delayed_recipient_chat = None
+    delayed_recipient_chat_title = None
+    delayed_recipient_delay_time = None
+
+    if should_send_delayed :
+        delayed_questions = [
+            inquirer.List('delayed_recipient', message='Select delayed recipient chat', choices=chat_choices),
+            inquirer.Text('delay_time', message='Type the time in seconds for the delay'),
+            inquirer.Text('delay_chance', message='Type the chance on percentage for the message be sent for the delayed chat'),
+            inquirer.Text('vip_link', message='Paste the VIP hire link')
+        ]
+        delayed_answers = answers = inquirer.prompt(delayed_questions)
+        delayed_recipient_chat_title = delayed_answers['delayed_recipient']
+        delayed_recipient_delay_time = int(delayed_answers['delay_time'])
+        delayed_recipient_delay_chance = int(delayed_answers['delay_chance']) * 0.01
+        delayed_recipient_vip_link = delayed_answers['vip_link']
+        delayed_recipient_chat = chat_dict[delayed_recipient_chat_title]
+
     return {
         'source_chats': source_chats,
         'source_chats_titles': source_chats_titles,
         'recipient_chat': recipient_chat,
         'recipient_chat_title': recipient_chat_title,
-        'should_sanitize': should_sanitize
+        'should_sanitize': should_sanitize,
+        'should_send_delayed': should_send_delayed,
+        'delayed_recipient_chat': delayed_recipient_chat,
+        'delayed_recipient_chat_title': delayed_recipient_chat_title,
+        'delayed_recipient_delay_time': delayed_recipient_delay_time,
+        'delayed_recipient_delay_chance': delayed_recipient_delay_chance,
+        'delayed_recipient_vip_link': delayed_recipient_vip_link
     }
 
+async def send_delayed_message(message, chat_settings):
+    random_number = random.random()
+    should_send_delayed_message = random_number >= chat_settings['delayed_recipient_delay_chance']
+    if should_send_delayed_message:
+        delayed_additional_message = message + '\n\nEsta mensagem foi enviada com ' + str(chat_settings['delayed_recipient_delay_time']) + ' segundos de atraso.\n\n[Assine o VIP](' + chat_settings['delayed_recipient_vip_link'] + ') para receber em tempo real.'
+        await asyncio.sleep(chat_settings['delayed_recipient_delay_time'])
+        await client.send_message(entity=chat_settings['delayed_recipient_chat'], message=delayed_additional_message, link_preview=False)
+
 load_dotenv()
-
-MONGO_URL = os.environ.get('MONGO_URL')
-
-mongo = MongoClient(MONGO_URL)
-db = mongo['data']
 
 TELEGRAM_API_ID = os.environ.get('TELEGRAM_API_ID')
 TELEGRAM_API_HASH = os.environ.get('TELEGRAM_API_HASH')
@@ -171,24 +184,34 @@ async def handle_new_message(event):
         if chat_settings['should_sanitize'] == True:
             message = sanitize_tipmanager_messages(message)
         
-        log_sent_message(message, chat_settings)
+        log_sent_message(message)
         if event.message.media is None or isinstance(event.message.media, types.MessageMediaWebPage):
             await client.send_message(entity=chat_settings['recipient_chat'], message=message, link_preview=False)
+            if chat_settings['should_send_delayed']:
+                await send_delayed_message(message, chat_settings)
         else:
             return await client.send_file(entity=chat_settings['recipient_chat'], file=event.message.media, caption=message)
     except Exception as e:
-        log_error(event.message.message, chat_settings, e)
+        log_error(e)
 
 @client.on(events.MessageEdited(chats=chat_settings['source_chats']))
 async def handle_message_edit(event):
     if chat_settings['should_sanitize'] == True:
         if "Resultado" in event.message.message:
             sanitized_edited_message = sanitize_tipmanager_messages(event.message.message)
+            
             async for message in client.iter_messages(chat_settings['recipient_chat']):
                 found_message = check_edited_message_equality(message.message, sanitized_edited_message)
                 if found_message:
                     sanitized_found_message = sanitize_tipmanager_messages_with_results(event.message.message)
                     await client.edit_message(chat_settings['recipient_chat'], message.id, sanitized_found_message)
+                    break
+            
+            async for message in client.iter_messages(chat_settings['delayed_recipient_chat'], limit=10):
+                found_message = check_edited_message_equality(message.message, sanitized_edited_message)
+                if found_message:
+                    sanitized_found_message = sanitize_tipmanager_messages_with_results(event.message.message)
+                    await client.edit_message(chat_settings['delayed_recipient_chat'], message.id, sanitized_found_message)
                     break
 
 client.run_until_disconnected()
